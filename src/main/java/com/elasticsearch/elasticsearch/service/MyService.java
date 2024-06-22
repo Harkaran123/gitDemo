4ae9a35d28ab311dea83a071fbfa4fa7d7f9e167
HappyGames <harkaransingh528@gmail.com>
2024-06-22 13:53:58 +0530

    Initial Commit

--- src/main/java/com/elasticsearch/elasticsearch/service/MyService.java
+++ src/main/java/com/elasticsearch/elasticsearch/service/MyService.java
@@ -5,8 +5,8 @@ public class MyService {
     public void myMethod(){
         System.out.println("Hii");
         int a  = 1;
-        System.out.println("I am from Master 1");
-        System.out.println("I am from Master 1");
+        System.out.println("I am from Feature 1");
+        System.out.println("I am from Feature 1");
 
     }
 }
