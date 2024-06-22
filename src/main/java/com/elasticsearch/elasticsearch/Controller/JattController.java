706843a0c9e7457b9685f79651e047093e340be1
HappyGames <harkaransingh528@gmail.com>
2024-06-22 12:16:01 +0530

    Initial Commit

---
+++ src/main/java/com/elasticsearch/elasticsearch/Controller/JattController.java
@@ -0,0 +1,12 @@
+package com.elasticsearch.elasticsearch.Controller;
+
+public class JattController {
+
+    public void myMethod(){
+        System.out.println("Hii");
+    }
+
+    public void yourMethod(){
+        System.out.printf("Hii Buddy");
+    }
+}
